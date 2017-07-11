81e78e4ecb089898e797ede6ad41d754d7bdeb09
Suraj Pai <suraj.rpai2093@gmail.com>
2017-07-11 13:20:09 +0530

    Add files via upload

--- Install-Apache.sh
+++ Install-Apache.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-apt-get -y update
+sudo apt-get -y update
 
 # install Apache2
-apt-get -y install apache2 
+sudo apt-get -y install apache2 
 
 # write some HTML
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
 
 # restart Apache
-apachectl restart
+sudo apachectl restart
